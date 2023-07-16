2748a671e5fc0a7710b87517206e1ba86079ebf7
AlexB <axb19c@acu.edu>
2023-07-16 00:18:38 -0500

    Adds package file

--- traveltracker/src/pages/index.js
+++ traveltracker/src/pages/index.js
@@ -1,20 +1,15 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import { Inter } from 'next/font/google';
 import styles from '@/styles/Home.module.css';
 import PrimaryButtonEnabled from '@/components/button-primary-enabled';
 import SecondaryButtonEnabled from '@/components/button-secondary-enabled';
 import { Main } from 'next/document';
 
-const inter = Inter({ subsets: ['latin'] });
-
 export default function Home() {
 	return (
 		<>
-			<div>
-				<PrimaryButtonEnabled />
-				<SecondaryButtonEnabled />
-			</div>
+			<PrimaryButtonEnabled />
+			<SecondaryButtonEnabled />
 		</>
 	);
 }
